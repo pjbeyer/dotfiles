c607d765323200ba0ef02f963cccd7ac8a6e5364
Phil Beyer <6152278+pjbeyer@users.noreply.github.com>
2023-11-23 09:19:56 -0500

    Create zshrc; Add antigen; Create shell functions

---
+++ zsh/settings.zsh
@@ -0,0 +1,24 @@
+# Load the oh-my-zsh library
+antigen use oh-my-zsh
+
+# Load bundles from the default repo (robbyrussell's oh-my-zsh)
+antigen bundle git
+
+# Load syntax highlighting bundle
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load OS-specific bundles
+case `uname` in
+  Darwin)
+    # macOS bundles
+    antigen bundle osx
+  ;;
+  Linux)
+    # Linux bundles
+  ;;
+esac
+
+# Load theme
+# antigen theme robbyrussell
+
+antigen apply
---
+++ zshrc
@@ -0,0 +1,11 @@
+# Load functions
+source ~/.shell/functions.sh
+
+# Update path
+source ~/.shell/path.sh
+
+# Load plugins
+source ~/.zsh/plugins/antigen.zsh
+
+# Load settings
+source ~/.zsh/settings.zsh
