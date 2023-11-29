382ab62a474dcd1259803f3a29c9eead43158316
Phil Beyer <6152278+pjbeyer@users.noreply.github.com>
2023-11-29 15:55:11 -0500

    Remove extraneous utilities

--- config/zsh/settings.zsh
+++ config/zsh/settings.zsh
@@ -1,14 +1,8 @@
 # Load the oh-my-zsh library
 antigen use oh-my-zsh
 
-# Load bundles from the default repo (robbyrussell's oh-my-zsh)
-antigen bundle git
-
 # Load syntax highlighting bundle
 antigen bundle zsh-users/zsh-syntax-highlighting
-
-# Load gitignore template bundle
-antigen bundle voronkovich/gitignore.plugin.zsh
 
 # Load OS-specific bundles
 case `uname` in
@@ -20,8 +14,5 @@ case `uname` in
     # Linux bundles
   ;;
 esac
-
-# Load theme
-# antigen theme robbyrussell
 
 antigen apply
--- zprofile
+++ zprofile
@@ -4,12 +4,6 @@ source ~/.config/shell/functions.sh
 # Update path
 source ~/.config/shell/path.sh
 
-# Load plugins
-source ~/.config/shell/plugins/chruby.sh
-
-# Load Zsh plugins
-source ~/.config/zsh/plugins/direnv.zsh
-
 # Allow local customization
 if [ -e ~/.config/zsh/zprofile ]; then
     source ~/.config/zsh/zprofile
