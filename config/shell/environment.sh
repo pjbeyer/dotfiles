ae030a051d265be662e0eebe42187d8ef1d4c480
Phil Beyer <6152278+pjbeyer@users.noreply.github.com>
2023-12-22 15:48:38 -0500

    set Python user install location

--- config/shell/environment.sh
+++ config/shell/environment.sh
@@ -4,3 +4,7 @@ export XDG_BIN_HOME=~/.local/bin
 export XDG_DATA_HOME=~/.local/share
 export XDG_STATE_HOME=~/.local/state
 export XDG_CACHE_HOME=~/.cache
+
+# Set location for the XCode Python "site.USER_BASE" variable
+# Add the "--user" option to `pip install`
+export PYTHONUSERBASE=~/.local
