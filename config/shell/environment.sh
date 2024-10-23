039adbb8e1c132dda45125c126c83ccd5c574bed
pjbeyer <6152278+pjbeyer@users.noreply.github.com>
2024-10-23 07:09:55 -0400

    add GnuPG settings

--- config/shell/environment.sh
+++ config/shell/environment.sh
@@ -5,6 +5,10 @@ export XDG_DATA_HOME=~/.local/share
 export XDG_STATE_HOME=~/.local/state
 export XDG_CACHE_HOME=~/.cache
 
+# GnuPG settings
+export GNUPGHOME=$XDG_CONFIG_HOME/gnupg
+export GPG_TTY=$(tty)
+
 # Set location for the XCode Python "site.USER_BASE" variable
 # Add the "--user" option to `pip install`
 export PYTHONUSERBASE=~/.local
